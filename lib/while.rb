fdead0a313ab50fd692bf3a3f8f8c5ba9af83734
Taylor Treece <tayloratreece@gmail.com>
2020-12-09 11:44:25 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,6 +1,6 @@
 def using_while
-  levitation_force = 0
-  while levitation_force < 11
+  levitation_force = 5
+  while levitation_force < 10
     puts 'Wingardium Leviosa'
     levitation_force += 1
   end
